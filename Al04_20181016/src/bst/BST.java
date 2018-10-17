42133a2365e3583c6d8bf0194d902dbc7ed831a5
yeji <aa960923@naver.com>
2018-10-17 23:35:51 +0900

    최종

--- Al04_20181016/src/bst/BST.java
+++ Al04_20181016/src/bst/BST.java
@@ -1,16 +1,32 @@
 package bst;
 
 public class BST {
-//	private int[] paramArr;
 	private int[] sortedArr;
 	private TreeNode root;
+	TreeNode current = root;
 
 	public BST(Integer[] list) {
 		this.root = null;
-//		paramArr = new int[list.length];
+		sortedArr = new int[list.length];
 		for (int i = 0; i < list.length; i++) {
 			insert(list[i].intValue());
+			sortedArr[i] = list[i].intValue();
 		}
+	}
+
+	public void resetRoot() {
+		this.root = null;
+	}
+
+	public void menu(String menu) {
+		if ("median".equals(menu)) {
+			quickSort(sortedArr, 0, sortedArr.length - 1);
+			median_insert(sortedArr, 0, sortedArr.length - 1);
+		}
+	}
+
+	public TreeNode getRoot() {
+		return this.root;
 	}
 
 	// 트리가 비어있는지 검사하는 함수
@@ -18,9 +34,19 @@ public class BST {
 		return root == null;
 	}
 
+	public void printTree(TreeNode node, int depth) {
+		if (node == null)
+			return;
+		for (int i = 0; i < depth; i++) {
+			System.out.print("\t");
+		}
+		System.out.println(node.data);
+		printTree(node.left, depth + 1);
+		printTree(node.right, depth + 1);
+	}
+
 	// 삽입
 	public void insert(int x) {
-
 		// 삽입할 노드 생성
 		TreeNode node = new TreeNode(x);
 
@@ -56,9 +82,19 @@ public class BST {
 		}
 	}
 
+	// median삽입
+	public void median_insert(int[] arr, int start, int end) {
+		if ((end - start + 1) < 1)
+			return;
+		int median = (end + start) / 2;
+		insert(arr[median]);
+		median_insert(arr, start, median - 1);
+		median_insert(arr, median + 1, end);
+	}
+
 	// 반복탐색
 	public int iterativeSearch(int key) {
-		TreeNode current = root;
+		current = root;
 		while (current != null) {
 			// 일치하는 노드를 찾았으면 리턴
 			if (current.data == key) {
@@ -78,7 +114,7 @@ public class BST {
 
 	// 순환탐색
 	public int recursiveSearch(int key) {
-		TreeNode current = root;
+		current = root;
 		while (current != null) {
 			// 일치하는 노드를 찾았으면 리턴
 			if (current.data == key) {
@@ -86,18 +122,20 @@ public class BST {
 			}
 			// 찾으려는 값이 비교 노드(현재 노드)보다 작으면 왼쪽으로 탐색
 			else if (current.data > key) {
-				return recursiveSearch(current.left.data);
+				root = current.left;
+				return recursiveSearch(key);
 			}
 			// 찾으려는 값이 비교 노드(현재 노드)보다 크면 오른쪽으로 탐색
 			else {
-				return recursiveSearch(current.right.data);
+				root = current.right;
+				return recursiveSearch(key);
 			}
 		}
 		return 0;
 	}
 
 	// 삭제
-	public boolean delete(int x) {
+	public boolean delete(String choice, int x) {
 		// 현재위치로부터의 루트 노드
 		TreeNode parent = root;
 		// 현재위치
@@ -157,20 +195,34 @@ public class BST {
 				parent.right = current.right;
 			}
 		}
-
-		// 3. 두개의 자식을 갖는 경우
+		// 3. 두개의 자식을 갖는 경우(successor & predecessor)
 		else if (current.left != null && current.right != null) {
-			// 오른쪽 서브트리의 최소값을 찾음
-			TreeNode successor = getSuccessor(current);
-			if (current == root) {
-				root = successor;
-			} else if (isLeftChild) {
-				parent.left = successor;
-			} else {
-				parent.right = successor;
-			}
+			if ("successor".equals(choice)) {
+				// 오른쪽 서브트리의 최솟값을 찾음
+				TreeNode successor = getSuccessor(current);
+				if (current == root) {
+					root = successor;
+				} else if (isLeftChild) {
+					parent.left = successor;
+				} else {
+					parent.right = successor;
+				}
 
-			successor.left = current.left;
+				successor.left = current.left;
+
+			} else if ("predecessor".equals(choice)) {
+				// 왼쪽 서브트리의 최댓값을 찾음
+				TreeNode predecessor = getPredecessor(current);
+				if (current == root) {
+					root = predecessor;
+				} else if (isLeftChild) {
+					parent.left = predecessor;
+				} else {
+					parent.right = predecessor;
+				}
+
+				predecessor.right = current.right;
+			}
 		}
 		return true;
 	}
@@ -194,18 +246,67 @@ public class BST {
 		return successor;
 	}
 
-	
+	// 선임자를 구하는 함수 (왼쪽 서브트리의 최댓값)
+	public TreeNode getPredecessor(TreeNode deleteNode) {
+		TreeNode predecessor = null;
+		TreeNode predecessorParent = null;
+		TreeNode current = deleteNode.left;
+
+		while (current != null) {
+			predecessorParent = predecessor;
+			predecessor = current;
+			// 오른쪽 서브트리를 타고 내려감
+			current = current.right;
+		}
+		if (predecessor != deleteNode.left) {
+			predecessorParent.right = predecessor.left;
+			predecessor.left = deleteNode.left;
+		}
+		return predecessor;
+	}
+
 	public void inorder() {
 		inorder(root);
 	}
 
 	private void inorder(TreeNode r) {
+
 		if (r != null) {
 			inorder(r.getLeft());
 			System.out.print(r.getData() + " ");
 			inorder(r.getRight());
 		}
 	}
-	
-	
+
+	// median insert 할 때 정렬하기 위한 퀵소트
+	private void swap(int arr[], int left, int right) {
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+	}
+
+	public void quickSort(int data[], int left, int right) {
+		if (left < right) {
+			int pivot = partition(data, left, right);
+			quickSort(data, left, pivot - 1);
+			quickSort(data, pivot + 1, right);
+		}
+	}
+
+	int partition(int data[], int left, int right) {
+		int pivot = right;
+
+		while (left < right) {
+			while (data[left] <= data[pivot] && left < right)
+				left++;
+			while (data[right] >= data[pivot] && left > right)
+				right--;
+			if (left < right)
+				swap(data, left, right);
+		}
+
+		swap(data, right, pivot);
+		return right; // 최종 오른쪽 피봇 반환
+	}
+
 }
--- Al04_20181016/src/bst/Main.java
+++ Al04_20181016/src/bst/Main.java
@@ -3,13 +3,47 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
+		TreeNode node=null;
 		Integer[] list= null;
 		BST bst = null;
 		try {
 			list=FileReading.readFileForList();
+			System.out.println("============== insert ===============");
 			bst=new BST(list);
+			bst.printTree(bst.getRoot(), 0);
+			bst.inorder();
+			System.out.println();
+			
+			System.out.println("============== median insert ===============");
+//			bst=new BST(list);
+			bst.resetRoot();
+			bst.menu("median");
+			bst.printTree(bst.getRoot(), 0);
 			bst.inorder();
 			
+			System.out.println();
+			
+			System.out.println("============== 자식노드 두 개 있는 32 search ===============");
+			bst=new BST(list);
+			System.out.println("iterativeSearch:"+bst.iterativeSearch(32));
+			System.out.println("recursiveSearch:"+bst.recursiveSearch(32));
+			System.out.println();
+			System.out.println("============== 자식노드 두 개 있는 34 delete ===============");
+			bst=new BST(list);
+			System.out.println("[successor]");
+			System.out.println(bst.delete("successor",34));
+			bst.printTree(bst.getRoot(), 0);
+			System.out.println();
+			bst=new BST(list);
+			System.out.println("[predecessor]");
+			System.out.println(bst.delete("predecessor",34));
+			bst.printTree(bst.getRoot(), 0);
+			System.out.println();
+			
+			System.out.println("============== 자식노드 한 개 있는 13 delete ===============");
+			bst=new BST(list);
+			System.out.println(bst.delete("successor",13));
+			bst.printTree(bst.getRoot(), 0);
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
